6d078acd8162fc71ec6248b5fe7c5609fabb5378
Documenter.jl <documenter@juliadocs.github.io>
2022-10-17 04:34:39 +0000

    build based on 5d907e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ClimaOcean.jl","page":"Home","title":"ClimaOcean.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Ocean component of CliMa's Earth system model.","category":"page"}]
+}
